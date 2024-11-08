e29ad24776fef6c0650b3182903547a70a52364a
novices666 <1601413678@qq.com>
2024-11-08 19:50:29 +0800

    update
    add api

--- XarrPay-Help/.vitepress/data/AsideData.ts
+++ XarrPay-Help/.vitepress/data/AsideData.ts
@@ -2,8 +2,8 @@
 
 export const Aside_Data = [
     {
-      link: 'https://auth.hg007.cc/?mod=shop&gid=3',
-      icon: 'https://auth.hg007.cc/assets/favicon.ico',
+      link: 'http://ksy.hg007.cc/goodslist?cid=12',
+      icon: 'https://xarr.hg007.cc/favicon.ico',
       Activity: '购买授权，享受优质服务',
       info1: '个人版：99/年、199/永久',
       info2: '商户版：299/年、699/永久'
