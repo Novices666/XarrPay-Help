846fc592795a2ab0f84687c62686ac0ca1f6361f
novices666 <1601413678@qq.com>
2024-12-12 13:15:30 +0800

    update

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -8,6 +8,9 @@ export default defineUserConfig({
   lang: "zh-CN",
   title: "XarrPay-Help",
   description: "基于GoLang的码支付系统帮助文档",
+  head:[
+    ['meta',{ name: 'keywords', content: 'XArrPay,XarrPay,xarrpay,官网,xarrpay官网,XArr码支付,Xarr码支付,授权,支付系统授权,码支付系统授权,帮助文档,码支付帮助文档,xarrpay帮助文档' }]
+  ],
   theme,
 
   // 和 PWA 一起启用
--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -4,7 +4,7 @@ import navbar from "./navbar.js";
 import sidebar from "./sidebar.js";
 
 export default hopeTheme({
-  hostname: "https://xarr.hg007.cc",
+  hostname: "https://doc.xarr.uk",
 
   author: {
     name: "HG007",
