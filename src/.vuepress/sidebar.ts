814859148f1de28adc1e26de277f7054a74a9a43
novices666 <1601413678@qq.com>
2025-01-11 15:29:36 +0800

    update

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -29,7 +29,8 @@ export default sidebar({
         "install",
         "template",
         "plugins",
-        "tools"
+        "tools",
+        "notify"
       ],
     },
   ],
--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -69,8 +69,8 @@ export default hopeTheme({
         content:"2025年起只保留新地址：<br><a href='https://doc.xarr.uk' target='_blank'>https://doc.xarr.uk</a><br><br>个人版：99/年付<br>商户版：699/三年、299/年付<br>点击跳转购买，享受站长优质售后",
         actions: [
           {
-            text: "新地址",
-            link: "https://doc.xarr.uk",
+            text: "论坛",
+            link: "https://bbs.xarr.uk",
             type: "primary",
           },
           {
